3276810d8b6e43e5049633a6ae60141f28cd63d1
milojezek <milo.jezek@gmail.com>
2024-01-05 23:55:23 +0100

    Fix letter counter

--- main.py
+++ main.py
@@ -12,13 +12,21 @@ def countLetters(text):
   letter_dict = {}
 
   lower_text = text.lower()
+
+  # Remove special characters
   for ch in ['\\','`','*','_','{','}','[',']','(',')','>','#','+',
              '-','.','!','$','\'', '/', '%', '@', ':', '?', ',', '"', ';', ' ']:
         if ch in lower_text:
             lower_text = lower_text.replace(ch, "")
-  lower_text = lower_text.replace("\n", "")
-  letters_numbers_only = lower_text
-  for char in letters_numbers_only:
+  letters_numbers_only = lower_text.replace("\n", "")
+
+  # Remove numbers
+  for n in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+     if n in letters_numbers_only:
+        letters_numbers_only = letters_numbers_only.replace(n, "")
+        
+  letters_only = letters_numbers_only
+  for char in letters_only:
     if char in letter_dict:
       letter_dict[char] += 1
     else:
