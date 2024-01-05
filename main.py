5100d8ca5693ddb3df4ba6dc7d887073cadd8fb2
milojezek <milo.jezek@gmail.com>
2024-01-05 23:51:05 +0100

    Add letter counter

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ def main():
   path_to_file = "books/frankenstein.txt"
   with open(path_to_file) as f:
     file_contents = f.read()
-  print(countWords(file_contents))
+  print(countLetters(file_contents))
 
 def countWords(text):
   words = text.split()
   return len(words)
+
+def countLetters(text):
+  letter_dict = {}
+
+  lower_text = text.lower()
+  for ch in ['\\','`','*','_','{','}','[',']','(',')','>','#','+',
+             '-','.','!','$','\'', '/', '%', '@', ':', '?', ',', '"', ';', ' ']:
+        if ch in lower_text:
+            lower_text = lower_text.replace(ch, "")
+  lower_text = lower_text.replace("\n", "")
+  letters_numbers_only = lower_text
+  for char in letters_numbers_only:
+    if char in letter_dict:
+      letter_dict[char] += 1
+    else:
+      letter_dict[char] = 1
+
+  return letter_dict
+
 
 if __name__ == "__main__":
   main()
